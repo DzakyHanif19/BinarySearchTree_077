81df8b50525f9a6f8914d79cecc94da305540e96
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-07-01 11:33:36 +0700

    jika isi data lebih kecil

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -45,6 +45,11 @@ public:
 			return; //exit
 		}
 
+		if (element < parent->info) //if the value in the data field of the new node is less than that of the parent
+		{
+			parent->leftchild = newNode; //make the left child of the parent point to the new node
+		}
+
 		
 	}
 
