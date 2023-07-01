840468f001ca34cd73f40d84b70e301fa686c99f
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-07-01 11:45:16 +0700

    postorder jika ptr tidak null

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -108,7 +108,12 @@ public:
 		{
 			cout << "Tree is empty" << endl;
 		}
-		
+		if (ptr != NULL)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
 	}
 };
 
